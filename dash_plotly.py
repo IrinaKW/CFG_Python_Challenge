2a86c6285d781dff8b62119647c3a8a847167300
Irina White <75076636+IrinaKW@users.noreply.github.com>
2023-05-01 11:37:41 +0200

    Update dash_plotly.py

--- dash_plotly.py
+++ dash_plotly.py
@@ -1,14 +1,17 @@
 from dash import Dash, html
 import pandas as pd
+import plotly.express as px
 
 # Incorporate data
-df = pd.read_csv('https://data/updated_.csv')
+df = pd.read_csv('data/updated_sales_dataset.csv')
 
 # Initialize the app
 app = Dash(__name__)
 
 app.layout = html.Div([
-    html.Div(children='Visual Representation')
+    html.Div(children='GFC-Challenge Visual Representation'),
+    dash_table.DataTable(data=df.to_dict('records'), page_size=10,
+    dcc.Graph(figure=px.histogram(df, x='continent', y='lifeExp', histfunc='avg'))
 ])
 
 
